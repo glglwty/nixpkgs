85ab2076bf32620d3aa67ac26cc5c2738341822c
Frederik Rietdijk <fridh@fridh.nl>
2018-05-28 14:16:02 +0200

    python: khal: 0.9.8 -> 0.9.9

--- pkgs/applications/misc/khal/default.nix
+++ pkgs/applications/misc/khal/default.nix
@@ -5,11 +5,11 @@ with python3Packages;
 buildPythonApplication rec {
   name = "${pname}-${version}";
   pname = "khal";
-  version = "0.9.8";
+  version = "0.9.9";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1blx3gxnv7sj302biqphfw7i6ilzl2xlmvzp130n3113scg9w17y";
+    sha256 = "50e7c40028c37d4ec76a3ef80e99af5f837ad6857590e76cc64ee29b16560937";
   };
 
   LC_ALL = "en_US.UTF-8";
